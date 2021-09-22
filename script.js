f515224d7f488c3002882f73f9efda32821c9b94
joacoARG <joaquincbarrera@gmail.com>
2021-09-22 17:07:39 -0300

    3 files modified

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log('hello!')
+let dayNight = document.querySelector('.dayNight');
+let menuToggle = document.querySelector('.menuToggle');
+let body = document.querySelector('body');
+
+dayNight.onclick = () => {
+  body.classList.toggle('dark');
+  dayNight.classList.toggle('active');
+};
+
+menuToggle.onclick = () => {
+  menuToggle.classList.toggle('active');
+};
